b61fc7515690e96e6666e95b0ff2936a0725f3f0
izeleneckiy <izeleneckiy@gmail.com>
2022-06-26 18:21:51 +0300

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+
+    min;
+    max;
+    mid;
+
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min + 1;
+        this.max = max - 1;
+        console.log('setRange max: ' + this.max + ' min: ' + this.min);
     }
 
     guess() {
-
+        console.log('guess:' + (this.min + (this.max - this.min) / 2));
+        this.mid = Math.round(this.min + (this.max - this.min) / 2);
+        return Math.round(this.min + (this.max - this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.mid - 1;
+        console.log('lower max: ' + this.max + ' min: ' + this.min);
     }
 
     greater() {
-
+        this.min = this.mid + 1;
+        console.log('greater max: ' + this.max + ' min: ' + this.min);
     }
 }
 
